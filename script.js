980f352ae3cd34b8128fc577e3750ffae25c91d8
PedroVSD <88330068+PedroVSD@users.noreply.github.com>
2023-01-20 12:39:19 -0300

    Primeiro projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+function recados(){
+    window.alert("Opá obrigado pela visita, essa é a primeira versão dessa página que você está vendo. É claro que ela precisa passar por alguns ajustes, como uma melhor estrutura de posicionamento e é claro o modo responsivo. Essa é a primeira página que faço, espero que tenha gostado.")
+}
+function imagens(){
+    window.alert("Direito de imagens")
+}
