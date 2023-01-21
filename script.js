761b5a50831d7e47cb53cfbb9b1271dfff03ecb3
PedroVSD <88330068+PedroVSD@users.noreply.github.com>
2023-01-21 17:28:12 -0300

    Melhorando a responsividade V(1.2)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function recados(){
-    window.alert("Opá obrigado pela visita, essa é a primeira versão dessa página que você está vendo. É claro que ela precisa passar por alguns ajustes, como uma melhor estrutura de posicionamento e é claro o modo responsivo. Essa é a primeira página que faço, espero que tenha gostado.")
+    window.alert("Opá obrigado pela visita, essa é a primeira versão dessa página que você está vendo. É claro que ela precisa passar por alguns ajustes, como uma melhor estrutura de posicionamento e é claro o modo responsivo. Espero que tenha gostado.")
 }
 function imagens(){
-    window.alert("Direito de imagens")
+    window.alert("Direito de imagens e icones: https://www.flaticon.com/br/icone-gratis/tecnologia_4257483 -- https://www.flaticon.com/br/icone-gratis/forma-de-pagamento_312136 -- https://www.flaticon.com/br/icone-gratis/protecao_4403096 -- https://www.flaticon.com/br/icone-gratis/web-design_3159310 -- https://www.flaticon.com/free-icon/smartwatch-app_3845840 -- https://www.flickr.com/photos/wiyre/26439242754 -- https://www.pexels.com/pt-br/foto/mulher-levantando-barra-1552249/ -- https://www.pexels.com/pt-br/foto/contador-balcao-guiche-maos-5239816/ -- https://www.pexels.com/pt-br/foto/computador-laptop-preto-e-cinza-546819/ -- https://br.freepik.com/fotos-gratis/onda-em-estrutura-de-arame-cinza-com-formas-geometricas-em-um-fundo-preto_17841634.htm#query=design%20grafico&position=5&from_view=search&track=sph")
 }
