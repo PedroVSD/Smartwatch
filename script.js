358307de476000789d6ac81887e33a9b9740096b
PedroVSD <88330068+PedroVSD@users.noreply.github.com>
2023-01-21 22:04:12 -0300

    V1.3 Responsividade no Menu estilo hamburguer(feito com CSS praticamente)

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function recados(){
 function imagens(){
     window.alert("Direito de imagens e icones: https://www.flaticon.com/br/icone-gratis/tecnologia_4257483 -- https://www.flaticon.com/br/icone-gratis/forma-de-pagamento_312136 -- https://www.flaticon.com/br/icone-gratis/protecao_4403096 -- https://www.flaticon.com/br/icone-gratis/web-design_3159310 -- https://www.flaticon.com/free-icon/smartwatch-app_3845840 -- https://www.flickr.com/photos/wiyre/26439242754 -- https://www.pexels.com/pt-br/foto/mulher-levantando-barra-1552249/ -- https://www.pexels.com/pt-br/foto/contador-balcao-guiche-maos-5239816/ -- https://www.pexels.com/pt-br/foto/computador-laptop-preto-e-cinza-546819/ -- https://br.freepik.com/fotos-gratis/onda-em-estrutura-de-arame-cinza-com-formas-geometricas-em-um-fundo-preto_17841634.htm#query=design%20grafico&position=5&from_view=search&track=sph")
 }
+/*function clickMenu(){
+    if(window.document.querySelector('i#hamburger').style.display == 'block'){
+        window.document.querySelector('i#hamburger').style.display = 'none'
+    } else {
+        window.document.querySelector('i#hamburger').style.display = 'block'
+    }
+}*/
